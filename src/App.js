610a7c52124152324d6a37c32b9236b8ec6dd121
unknown <kanbayoguzhan@gmail.com>
2022-08-26 23:06:29 +0300

    setting up project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import UserSignUp from './components/UserSignup';
+
+function App() {
+  return (
+   <div>
+   <UserSignUp/>
+
+   </div>
+     
+  );
+}
+
+export default App;
